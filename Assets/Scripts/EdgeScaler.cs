a635d3aa5502df120e41e087db225e5590028982
DatLee <ledat@smartwave247.com>
2024-10-07 13:55:32 +0700

    + fix player pos

--- Assets/Scripts/EdgeScaler.cs
+++ Assets/Scripts/EdgeScaler.cs
@@ -1,22 +1,30 @@
 ﻿using UnityEngine;
 
 public class EdgeScaler : MonoBehaviour {
-    [SerializeField] GameObject playerPos;
-    [SerializeField] GameObject bottomEdge;
-    public float distanceToMove ;
+    [SerializeField] GameObject playerPos; // Gán đối tượng người chơi
+    [SerializeField] GameObject bottomEdge; // Gán cạnh dưới
+    public float distanceToMove; // Khoảng cách để di chuyển người chơi
 
     void Start() {
+        // Lấy kích thước màn hình
         float screenHeight = Camera.main.orthographicSize * 2;
         float screenWidth = screenHeight * Camera.main.aspect;
-        if(gameObject.tag == "BottomEdge")
-        playerPos.transform.position = new Vector2(playerPos.transform.position.x, bottomEdge.transform.position.y + distanceToMove);
+
+        // Đặt vị trí cho người chơi cách cạnh dưới một khoảng distanceToMove
+        if (gameObject.CompareTag("BottomEdge")) {
+            playerPos.transform.position = new Vector2(playerPos.transform.position.x, bottomEdge.transform.position.y + distanceToMove);
+        }
 
         // Điều chỉnh kích thước cho các cạnh
-        if (gameObject.CompareTag("TopEdge") || gameObject.CompareTag("BottomEdge")|| gameObject.CompareTag("Player")) {
+        if (gameObject.CompareTag("TopEdge") || gameObject.CompareTag("BottomEdge")) {
+            // Kéo dài cạnh theo chiều ngang
             transform.localScale = new Vector3(screenWidth, transform.localScale.y, transform.localScale.z);
-            transform.position = new Vector3(0, (gameObject.CompareTag("TopEdge") ? screenHeight / 2 : -screenHeight / 2), 0);
+            // Đặt vị trí cạnh trên hoặc dưới
+            transform.position = new Vector3(0, (gameObject.CompareTag("TopEdge") ? screenHeight / 2 : -screenHeight / 2 + 1f), 0);
         } else if (gameObject.CompareTag("LeftEdge") || gameObject.CompareTag("RightEdge")) {
+            // Kéo dài cạnh theo chiều cao
             transform.localScale = new Vector3(transform.localScale.x, screenHeight, transform.localScale.z);
+            // Đặt vị trí cạnh trái hoặc phải
             transform.position = new Vector3((gameObject.CompareTag("LeftEdge") ? -screenWidth / 2 : screenWidth / 2), 0, 0);
         }
     }
